c878b941cdadb95385679aafc238e6ad61a2b972
Davronbek Achilov <davron211@gmail.com>
2021-02-21 20:56:56 +0500

    message re upload

--- resources/views/emails/request/reupload.blade.php
+++ resources/views/emails/request/reupload.blade.php
@@ -5,12 +5,17 @@
 </head>
 <body>
 
-<h1>Maqolangiz to'g'risida xabar:</h1>
+<h1>Maqolangizni qayta yuklash haqida:</h1>
 
 <p>
-    Assalomu aleykum.
-    Sizning maqolangizda kamchiliklar mavjud. Qayta yuklamasangiz sizning maqolangiz qabul qilinmasligi mumkin.
-    {{ $text }}
+    Assalomu aleykum.<br>
+    Sizning maqolangizda kamchiliklar mavjud.<br>
+    Qayta yuklamasangiz sizning maqolangiz qabul qilinmasligi mumkin.<br>
+    <h3>Mutaxassis javobi:</h3> <b>{{ $text }}</b>
+</p>
+<p>
+    <span style="color: #b3710d;">Qayta yuklash uchun quyidagi havolaga bosing:</span><br>
+    <a href="http://conferences-list.uz/change?hash={{ $hash }}">http://conferences-list.uz/change?hash={{ $hash }}</a>
 </p>
 </body>
 </html>
